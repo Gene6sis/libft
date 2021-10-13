c8d6799eedbaeb1d94c890effab2e667f148f558
Adam Toons <adam@MacBook-Air-de-Adam.local>
2021-10-13 12:46:05 +0200

    v

--- done/ft_strlcat.c
+++ done/ft_strlcat.c
@@ -6,19 +6,11 @@
 /*   By: adben-mc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 04:39:13 by adben-mc          #+#    #+#             */
-/*   Updated: 2021/09/08 05:13:30 by adben-mc         ###   ########.fr       */
+/*   Updated: 2021/10/13 12:35:09 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 size_t	ft_strlcat(char *restrict dst,
 		const char *restrict src, size_t dstsize)
--- done/ft_strlcpy.c
+++ done/ft_strlcpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
