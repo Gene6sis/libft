109945879c2be9ee220fd576711e3f6d6671bc9a
Adam BEN MCHICHI <adben-mc@e2r6p1.clusters.42paris.fr>
2021-11-23 15:08:34 +0100

    update

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -14,8 +14,7 @@
 
 int	ft_isalnum(int c)
 {
-	if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
-			|| (c >= 'A' && c <= 'Z')))
-		return (0);
-	return (1);
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
 }
