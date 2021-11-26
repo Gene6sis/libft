b666a1b675c8665fe4fe95ed634806ee0bbb4304
Adam BEN MCHICHI <adben-mc@e2r7p7.clusters.42paris.fr>
2021-11-26 19:14:54 +0100

    update

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,18 @@
 /*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:35:45 by adben-mc          #+#    #+#             */
-/*   Updated: 2021/11/23 17:35:46 by adben-mc         ###   ########.fr       */
+/*   Updated: 2021/11/26 19:14:27 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	list;
+
+	if (!content)
+		return (NULL);
+	list.content = malloc(sizeof(*content));
+	return (list);
+}
